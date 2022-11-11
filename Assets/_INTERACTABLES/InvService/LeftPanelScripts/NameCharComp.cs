731811bee8ab1d60a5c11028aa58311038c47760
SachinSuperCoder <contact.ssingh@gmail.com>
2022-11-11 19:38:28 +0530

    inv gif

---
+++ Assets/_INTERACTABLES/InvService/LeftPanelScripts/NameCharComp.cs
@@ -0,0 +1,30 @@
+using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+namespace Interactables
+{
+    public class NameCharComp : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI nametxt; 
+        void Start()
+        {
+            nametxt = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            InvService.Instance.OnCharSelectInPanel += PrintName; 
+           
+        }
+
+        void PrintName(CharModel charModel)
+        {
+            nametxt.text = charModel.charNameStr;
+
+        }
+
+      
+    }
+
+
+
+}
